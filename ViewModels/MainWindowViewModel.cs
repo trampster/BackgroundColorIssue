5120656fb41020cce844ae71e9c9cd0a5b94b292
Daniel Hughes <trampster@gmail.com>
2022-02-19 21:14:42 +1300

    Add code to reproduce the background color binding issue.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Media;
+
+namespace BackgroundColorIssue.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public Brush BackgroundColor = new SolidColorBrush(Colors.Red);
+
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BackgroundColorIssue.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
